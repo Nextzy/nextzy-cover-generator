ae0eb30e8c5b146ad5f502cd90187de91cddeb33
Akexorcist <akexorcist@gmail.com>
2019-03-17 03:17:36 +0700

    Updates

---
+++ precache-manifest.ab68e9a1704d99b103011a31489c709f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a06d0eafcd4f02c82aa7",
+    "url": "/Nextzy/nextzy-cover-generator/static/css/main.e9f50b75.chunk.css"
+  },
+  {
+    "revision": "a06d0eafcd4f02c82aa7",
+    "url": "/Nextzy/nextzy-cover-generator/static/js/main.de89fac1.chunk.js"
+  },
+  {
+    "revision": "9b2c4d1140f90079de57",
+    "url": "/Nextzy/nextzy-cover-generator/static/js/runtime~main.e54b759a.js"
+  },
+  {
+    "revision": "5523b978e5796edb2420",
+    "url": "/Nextzy/nextzy-cover-generator/static/css/2.d59217fb.chunk.css"
+  },
+  {
+    "revision": "5523b978e5796edb2420",
+    "url": "/Nextzy/nextzy-cover-generator/static/js/2.cadc7697.chunk.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/Nextzy/nextzy-cover-generator/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "87496d12ed5e337ff096b5d69cda9189",
+    "url": "/Nextzy/nextzy-cover-generator/index.html"
+  }
+];
